51eda2b49e2e5d56de9d84e8db716fa99b73a02f
raesp.berry <98147245+theRaeRae@users.noreply.github.com>
2023-05-18 10:00:25 +0800

    Giving the right name

---
+++ namingConvention.js
@@ -0,0 +1,5 @@
+//Create a variable with the name of our planet
+let nameOfOurPlanet = "Earth";
+
+//Create a variable to store the name of a current visitor to a website
+let newVisitor = "Aliens";
