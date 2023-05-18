29d7337e9907cffcdc3cdf26b7b2e24dd1544942
raesp.berry <98147245+theRaeRae@users.noreply.github.com>
2023-05-18 10:34:38 +0800

    Not a Number

---
+++ NaN.js
@@ -0,0 +1,6 @@
+//NaN is a JS reserved word indicating that a number is not a legal number
+
+let x = 100 / "Apple"; //will result into NaN
+
+let y = 100 / "10"; //will work because the string is numeric
+
