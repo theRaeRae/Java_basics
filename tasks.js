3c1296222859042869020fb3c85dc353ef18e552
raesp.berry <98147245+theRaeRae@users.noreply.github.com>
2023-05-18 09:57:10 +0800

    Working with variables

---
+++ tasks.js
@@ -0,0 +1,7 @@
+let admin, name; //Declare variables called admin and name
+
+name = "John"; //Assigned "John" to name
+
+admin = name; //Copied the value of name to admin
+
+alert(admin); //Show the value of admin
