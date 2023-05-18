96a7653db77c2fb164f8e436c78368c7f90efc6c
raesp.berry <98147245+theRaeRae@users.noreply.github.com>
2023-05-18 10:38:16 +0800

    infinity number

---
+++ infinity.js
@@ -0,0 +1,10 @@
+//Infinity is the largest number available
+
+let myNumber = 2;
+// Execute until Infinity
+while (myNumber != Infinity) {
+  myNumber = myNumber * myNumber;
+}
+//the code will run until it hits the highest number possible in JS
+
+//Dividing by 0 also results in infinity
